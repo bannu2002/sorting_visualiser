3cbe4ff22e0415e1d8b69b01905c44970f587e71
bannu2002 <87083443+bannu2002@users.noreply.github.com>
2022-05-16 02:37:05 +0530

    Update visualizations.js

--- scripts/visualizations.js
+++ scripts/visualizations.js
@@ -1,7 +1,7 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
-    E-MAIL:-    tummala.kethan@gmail.com
+    DONE BY:-   kanigiri guru dattathreya
+    E-MAIL:-    gurudattu2002@gmail.com
     *****************
 */
 
@@ -59,7 +59,7 @@ function enable_buttons()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
-    E-MAIL:-    tummala.kethan@gmail.com
+    DONE BY:-   kanigiri guru dattathreya
+    E-MAIL:-    gurudattu2002@gmail.com
     *****************
 */
