835a4aaf2471010b1d8f5e3795d4fc696ec44e3e
bannu2002 <87083443+bannu2002@users.noreply.github.com>
2022-05-16 02:33:23 +0530

    Update merge_sort.js

--- scripts/merge_sort.js
+++ scripts/merge_sort.js
@@ -1,7 +1,7 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
-    E-MAIL:-    tummala.kethan@gmail.com
+    DONE BY:-   kanigiri guru dattathreya
+    E-MAIL:-    gurudattu2002@gmail.com
     *****************
 */
 
@@ -67,7 +67,7 @@ function merge_partition(start,end)
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
-    E-MAIL:-    tummala.kethan@gmail.com
+    DONE BY:-   kanigiri guru dattathreya
+    E-MAIL:-    gurudattu2002@gmail.com
     *****************
 */
